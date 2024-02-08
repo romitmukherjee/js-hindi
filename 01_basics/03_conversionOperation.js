db72229c1564c1e95f4ef2dbbca6807ac61328f0
romitmukherjee <101014713+romitmukherjee@users.noreply.github.com>
2024-02-08 18:20:47 +0000

    datatypes-summary

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof  valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 - true 0- false in boolean
+
+//let someNumber = 33
+
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//******************OPERATIONS*******************//
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+
+
+let str1 = "hello"
+let str2 = "romit"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id == anotherId);
+
+//const bigNumber = 12334567899123334432n
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name:"romit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros);
