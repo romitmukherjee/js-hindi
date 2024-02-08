b5d1aa508e86928ba200fb20caacf34247053572
romitmukherjee <101014713+romitmukherjee@users.noreply.github.com>
2024-02-08 05:11:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+console.log(3+3)
+
+console.log("romit")
+
+let name = "romit"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number = 2 to the power 53
+// bigInt = lagrge number
+// string = ""
+// boolean = true/false
+// null = standalone vaslue
+// undefined = 
+// symbol = unique
+
+
+// object
+
+console.log(typeof "hitesh")
